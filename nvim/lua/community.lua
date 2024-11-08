78ae26aaa3331f0f56f072e66e1dfe50cb01ad89
bill gates <bill@gates.com>
2024-11-08 22:16:46 +0100

    plugins

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -6,4 +6,8 @@ return {
   { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.markdown-and-latex.render-markdown-nvim" },
   { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.color.twilight-nvim" },
+  -- { import = "astrocommunity.utility.neodim" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
 }
