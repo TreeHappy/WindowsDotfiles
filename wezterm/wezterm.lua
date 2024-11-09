ea629a623c34ec31db2f32e3993c2e2e99eb856c
bill gates <bill@gates.com>
2024-11-09 16:08:25 +0100

    wezterm settings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,14 +4,24 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.default_prog = { "pwsh.exe" }
-config.window_background_opacity = 0.6
+config.default_prog = { "pwsh.exe", "-NoLogo" }
+config.window_background_opacity = 0.9
 config.color_scheme = "Catppuccin Mocha"
 -- config.color_scheme = "Campbell (Gogh)"
 config.font_size = 22.0
 config.hide_tab_bar_if_only_one_tab = true
 -- config.window_decorations = "RESIZE"
 config.font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = false })
+
+config.window_padding = {
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
+}
+
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
 
 -- and finally, return the configuration to wezterm
 return config
