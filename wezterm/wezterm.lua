11606552b44c8211b8314a380de4afd82ae451d9
bill gates <bill@gates.com>
2024-11-15 20:00:00 +0100

    settings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -30,6 +30,7 @@ config.tab_bar_at_bottom = true
 config.show_tab_index_in_tab_bar = false
 config.show_new_tab_button_in_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = false
+config.default_cursor_style = "BlinkingBar"
 
 local tab_colors = {
 	"Navy",
