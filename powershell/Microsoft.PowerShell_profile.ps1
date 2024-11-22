27a1b61709224f18cd27043ec5f266e348a49116
bill gates <bill@gates.com>
2024-11-22 07:59:16 +0100

    settings

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,6 @@
+$env:EDITOR = "nvim"
+$env:YAZI_FILE_ONE = "C:\Program Files\Git\usr\bin\file.exe"
+$env:YAZI_CONFIG_HOME = Join-Path $env:HOME ".config/yazi/"
 $env:XDG_CONFIG_HOME = Join-Path $env:HOME ".config"
 
 Invoke-Expression (&starship init powershell)
@@ -5,4 +8,5 @@ Invoke-Expression (&starship init powershell)
 carapace git powershell | Out-String | Invoke-Expression
 carapace _carapace powershell | Out-String | Invoke-Expression
 carapace gh powershell | Out-String | Invoke-Expression
+carapace go powershell | Out-String | Invoke-Expression
 
